8d51d6c7298122db00369a8e5b0bdd941d296066
hung981vpp <hung981vp@gmail.com>
2026-05-28 16:18:21 +0700

    Fix Render PostgreSQL DATABASE_URL port parsing

--- Program.cs
+++ Program.cs
@@ -90,8 +90,9 @@ static string BuildPostgresConnectionString(IConfiguration configuration)
         var username = Uri.UnescapeDataString(userInfo[0]);
         var password = userInfo.Length > 1 ? Uri.UnescapeDataString(userInfo[1]) : string.Empty;
         var database = uri.AbsolutePath.TrimStart('/');
+        var port = uri.Port > 0 ? uri.Port : 5432;
 
-        return $"Host={uri.Host};Port={uri.Port};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+        return $"Host={uri.Host};Port={port};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true";
     }
 
     return configuration.GetConnectionString("MedicalDB")
